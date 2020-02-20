afa5a704fba7ff690c5361e794dc4f6460c8164f
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-02-20 17:49:36 -0300

    Substituição dos parâmetros soltos de paginação e ordenação por um parâmetro do objeto Pageable para simplificação, adicionada a anotação @EnableSpringDataWebSupport no ForumApplication e definidos com @PageableDefault valores padrão caso não haja passagem de parâmetros em url pelo cliente

--- forum/src/main/java/br/com/alura/forum/ForumApplication.java
+++ forum/src/main/java/br/com/alura/forum/ForumApplication.java
@@ -2,11 +2,14 @@ package br.com.alura.forum;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
-public class ForumApplication {
-
-	public static void main(String[] args) {
+@EnableSpringDataWebSupport
+public class ForumApplication
+{
+	public static void main(String[] args)
+	{
 		SpringApplication.run(ForumApplication.class, args);
 	}
 
