3bf2e829d9886a664fb26db906551892f4e7a83b
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-02-22 17:52:44 -0300

    Invalidação do cache para caso de atualização das informações cadastradas na API, utilizando a anotação @CacheEvict nos métodos necessários da classe TopicosController com seus respectivos parâmetros.

--- forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ forum/src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -12,6 +12,7 @@ import br.com.alura.forum.modelo.Curso;
 import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -58,6 +59,7 @@ public class TopicosController
 
     @PostMapping
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> cadastrar(@RequestBody @Valid TopicoForm form, UriComponentsBuilder uriBuilder)
     {
         Topico topico = form.converter(cursoRepository);
@@ -80,6 +82,7 @@ public class TopicosController
 
     @PutMapping("/{id}")
     @Transactional //necessário para atualizar no banco de dados
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<TopicoDto> atualizar(@PathVariable Long id, @RequestBody @Valid AtualizacaoTopicoForm form)
     {
         Optional<Topico> optional = topicoRepository.findById(id);
@@ -93,6 +96,7 @@ public class TopicosController
 
     @DeleteMapping("/{id}")
     @Transactional
+    @CacheEvict(value = "listaDeTopicos", allEntries = true)
     public ResponseEntity<?> remover(@PathVariable Long id)
     {
         Optional<Topico> optional = topicoRepository.findById(id);
