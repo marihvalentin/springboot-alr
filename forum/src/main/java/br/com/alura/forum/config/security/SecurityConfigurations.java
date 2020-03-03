0645aaf54542d6b9bab14489d156e0ea2f0ecbfd
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-03-03 18:12:17 -0300

    Realizada as implementações necessárias para configurar a autenticação para acesso as endpoints privadas.

--- forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ forum/src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -1,5 +1,6 @@
 package br.com.alura.forum.config.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,27 +8,32 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfigurations extends WebSecurityConfigurerAdapter
 {
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
     //configurações de autenticação
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception
     {
-
+        auth.userDetailsService(autenticacaoService).passwordEncoder(new BCryptPasswordEncoder());
     }
 
     //configurações de autorização (urls do projeto)
     @Override
     protected void configure(HttpSecurity http) throws Exception
-    {   //liberando acesso aos endpoints públicos (metodo lista e detalhar que utilizam o GET)
+    {
+        //liberando acesso aos endpoints públicos (metodo lista e detalhar que utilizam o GET)
         http.authorizeRequests()
                 .antMatchers(HttpMethod.GET, "/topicos").permitAll()
                 .antMatchers(HttpMethod.GET, "topicos/*").permitAll()
                 .anyRequest().authenticated()
-        .and().formLogin();
+                .and().formLogin();
     }
 
     //configurações de recursos estáticos (js, css, imagens, etc...)
--- forum/src/main/resources/data.sql
+++ forum/src/main/resources/data.sql
@@ -1,4 +1,4 @@
-INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '123456');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com', '$2a$10$oJDW/.P1SkX5pozPzcLY9OE4ht7/4kknRZZB98Xn1thJ052LWSJ3C');
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
