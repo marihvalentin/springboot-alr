efe189ac02c94a37b2a5aea4b8f6fc1438a09614
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-03-08 17:39:38 -0300

    Classe service adicionada no package Security com as configurações e propriedades definidas para a geração de tokens a partir da JJWT

--- forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
+++ forum/src/main/java/br/com/alura/forum/config/security/TokenService.java
@@ -1,4 +1,38 @@
 package br.com.alura.forum.config.security;
 
-public class TokenService {
+import br.com.alura.forum.modelo.Usuario;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
+public class TokenService
+{
+    @Value("${forum.jwt.expiration}")
+    private String expiration;
+
+    @Value("${forum.jwt.secret}")
+    private String secret;
+
+    public String gerarToken(Authentication authentication)
+    {
+        Usuario logado = (Usuario) authentication.getPrincipal();
+        Date hoje = new Date();
+        //expiração = data hoje + o tempo de expiração que está como propriedade no application.properties
+        Date dataExpiracao = new Date(hoje.getTime() + Long.parseLong(expiration));
+
+        return Jwts.builder()
+                .setIssuer("API do Fórum da Alura") //Quem está gerando o token?
+                .setSubject(logado.getId().toString()) //Quem é o usuário autenticado dono do token?
+                .setIssuedAt(hoje) //Data de geração do token
+                .setExpiration(dataExpiracao) //Data de expiração do token
+                //Algoritmo de cripto + senha da aplicação usada p/ fazer a assinatura e gerar o REST da cripto do token
+                .signWith(SignatureAlgorithm.HS256,secret)
+                .compact(); //compactar o retorno em uma string
+
+    }
 }
