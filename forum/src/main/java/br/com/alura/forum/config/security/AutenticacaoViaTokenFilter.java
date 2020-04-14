d4b6b35e4d3f12a0fcfc34d124ea4d6560393bb9
Mariana Valentim <marihvalentin_2@hotmail.com>
2020-04-14 17:41:58 -0300

    Criada a classe tipo filter AutenticacaoViaTokenFilter e realizado seu registro no método configure na classe SecurityConfigurations para realizar nesse primeiro momento a recuperação do token fornecido na autenticação do cliente na API

--- forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
+++ forum/src/main/java/br/com/alura/forum/config/security/AutenticacaoViaTokenFilter.java
@@ -1,4 +1,35 @@
 package br.com.alura.forum.config.security;
 
-public class AutenticacaoViaTokenFilter {
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AutenticacaoViaTokenFilter extends OncePerRequestFilter
+{
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException
+    {
+        //recuperar o token
+        String token = recuperarToken(request);
+        System.out.println(token);
+
+        filterChain.doFilter(request, response);
+    }
+
+    private String recuperarToken(HttpServletRequest request)
+    {
+        String token = request.getHeader("Authorization");
+
+        if(token == null || token.isEmpty() || !token.startsWith("Bearer "))
+        {
+            return null;
+        }
+
+        return token.substring(7, token.length());
+    }
 }
